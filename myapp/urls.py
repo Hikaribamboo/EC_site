f7918d6c2ea46951bc2277eda5fa02f47b7c2c0f
Hikaribamboo <hikari1653@gmail.com>
2024-12-19 00:36:05 +0900

    established login
    wq

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,15 +7,13 @@ urlpatterns = [
     # 空のパスをメイン画面にリダイレクト
     path('', views.main_page, name='main'),
 
-
     # 管理者用
     path('products/admin/', views.admin_product_list, name='admin_product_list'),  # 管理者用商品リスト
     path('products/delete/<str:product_id>/', views.delete_product, name='delete_product'),  # 商品削除
     path('product/add/', views.product_add, name='product_add'),  # 出品ページ
-    
+
     # 一般ユーザー用
     path('products/', views.product_list_user, name='product_list_user'),  # 商品リスト
-    path('notifications/', views.notification_list, name='notification_list'),  # 通知リスト
     path('main/', views.main_page, name='main'),  # メイン画面
     
     # ログイン用
@@ -24,6 +22,9 @@ urlpatterns = [
 
     path('register/', views.register, name='register'),
     path('verify_qr/', views.verify_qr, name='verify_qr'),
+
+    path('mypage/', views.mypage, name='mypage'),  # マイページ
+    path('notifications/', views.notifications, name='notifications'),  # 通知ページ
 ]
 
 
