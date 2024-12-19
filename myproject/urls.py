ae8eadf29c68bdba3a7b02c9a2e5cf0afb263fbe
Hikari <143927390+Hikaribamboo@users.noreply.github.com>
2024-12-19 13:29:56 +0900

    Delete myproject directory

--- myproject/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include  # include をインポート
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),  # myapp の urls.py を参照
-]
